963de3c5f8da4e27c085f986fa2aad7e5cbce26e
cs99alpha <schaudhary3@hawk.iit.edu>
2021-05-18 10:13:01 -0700

    Add project files.

---
+++ ConsoleApp1/Program.fs
@@ -0,0 +1,13 @@
+﻿// Learn more about F# at http://fsharp.org
+
+//open System
+module HelloSquare
+
+let square x = x * x
+
+[<EntryPoint>]
+let main argv =
+    //printfn "Hello World from F#!"
+    printfn "%d squared is: %d!" 12 (square 12)
+    0 // return an integer exit code
+
